b54d8c62b9cb94b08114354b67e685ca98ab9f4b
thinktank01 <felixayot@gmail.com>
2024-03-26 23:40:27 +0300

    Add Application and Role db models in the shell context

--- heza.py
+++ heza.py
@@ -3,10 +3,16 @@ from api import create_app
 from api.utils.db import db
 from api.models.users import User
 from api.models.job_posts import JobPost
+from api.models.applications import Application
 
 
 app = create_app()
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'JobPost': JobPost}
+    return {
+        'db': db,
+        'User': User,
+        'JobPost': JobPost,
+        'Application': Application
+        }
